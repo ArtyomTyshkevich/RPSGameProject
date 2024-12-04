94a9fcd4c49e0d61d9c7bd603d495254cd6e2e5b
Artyom Tyshkevich <frod_1999@mail.ru>
2024-12-04 19:36:08 +0300

    Implement Chat v.1

---
+++ Backend/Microservices/ChatService/ChatService/Chat.Data/Repositories/MessageRepository.cs
@@ -0,0 +1,41 @@
+﻿using Chat.Application.DTOs;
+using Chat.Application.Interfaces;
+using Chat.Data.Context;
+using Chat.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+
+namespace Chat.Data.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly IMongoCollection<Message> _messages;
+
+        public MessageRepository(MongoDbContext context)
+        {
+            _messages = context.Messages;
+        }
+
+        public async Task<IEnumerable<Message>> GetMessagesAsync(string chatRoom)
+        {
+            return await _messages.Find(m => m.Content.Contains(chatRoom)).ToListAsync();
+        }
+
+        public async Task SaveMessageAsync(Message message)
+        {
+            await _messages.InsertOneAsync(message);
+        }
+        public Message Create(UserDTO userDTO, string messageContent)
+        {
+            return new Message
+            {
+                UserId = userDTO.Id,
+                UserName = userDTO.NickName,
+                Content = messageContent,
+                SentAt = DateTime.UtcNow
+            };
+
+        }
+
+    }
+}
---
+++ Backend/Microservices/ChatService/ChatService/Chat.WebAPI/Hubs/ChatHub.cs
@@ -0,0 +1,55 @@
+﻿using Chat.Application.DTOs;
+using Chat.Application.Interfaces;
+using Chat.Domain.Entities;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Chat.API.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDistributedCache _cache;
+        private readonly ICacheService _cacheService;
+
+        public ChatHub(IUnitOfWork unitOfWork, IDistributedCache cache, ICacheService cacheService)
+        {
+            _cache = cache;
+            _cacheService = cacheService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task JoinChat(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+            await _cacheService.CachingConnection(Context.ConnectionId, connection);
+        }
+
+        public async Task SendMessage(string messageContext)
+        {
+
+            var connection = await _cacheService.GetConnectionFromCache(Context.ConnectionId);
+            if (connection != null)
+            {
+                var message = _unitOfWork.Messages.Create(connection.UserDTO,messageContext);
+                await Clients
+                    .Group(connection.ChatRoom)
+                    .ReceiveMessage(connection.UserDTO, message);
+
+            }
+
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var connection = await _cacheService.GetConnectionFromCache(Context.ConnectionId);
+            if (connection != null)
+            {
+                await _cache.RemoveAsync(Context.ConnectionId);
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
