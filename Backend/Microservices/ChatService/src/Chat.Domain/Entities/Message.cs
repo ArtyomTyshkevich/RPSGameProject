c5e5f6a7cc3d4343d529b659ebc638aea4511a2c
Тышкевич Артем <frod_1999@mail.ru>
2024-12-09 17:20:35 +0300

    refactoring

--- Backend/Microservices/ChatService/src/Chat.Domain/Entities/Message.cs
+++ Backend/Microservices/ChatService/src/Chat.Domain/Entities/Message.cs
@@ -11,14 +11,18 @@ namespace Chat.Domain.Entities
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
+
         [BsonElement("userId")]
         public Guid UserId { get; set; } = Guid.NewGuid();
+
 
         [BsonElement("userName")]
         public string UserName { get; set; } = "";
 
+
         [BsonElement("content")]
         public string Content { get; set; } = "";
+
         [BsonElement("SentAt")]
         public DateTime SentAt { get; set; } = DateTime.UtcNow;
     }
